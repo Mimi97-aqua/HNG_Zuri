5c16f9bc72c1479fdfb6cb4af192ca421116bd5b
Mimi97-aqua <ebenyemiriam17@gmail.com>
2023-09-14 18:40:04 +0100

    CREATE: Adding a new person API

--- stage_two_task/app.py
+++ stage_two_task/app.py
@@ -1,9 +1,35 @@
-from flask import jsonify
+from flask import jsonify, request
 from data.resource import *
 import json
 
 
-# Fetch details of a person
+# CREATE: Adding a new person
+@app.route('/api', methods=['GET', 'POST'])
+def create():
+    if request.method == 'GET':
+        persons = Person.query.all()
+        data = [person.serialize() for person in persons]
+        result = json.dumps(data, indent=4)
+        return result, 200, {'Content-Type': 'application/json'}
+    elif request.method == 'POST':
+        name = request.json.get('name')
+        gender = request.json.get('gender')
+        email = request.json.get('email')
+        age = request.json.get('age')
+        weight = request.json.get('weight')
+
+        person_new = Person(name=name, gender=gender, email=email, age=age, weight=weight)
+        db.session.add(person_new)
+        db.session.commit()
+
+        data = person_new.serialize()
+        result = json.dumps(data, indent=4)
+        return result, 201, {'Content-Type': 'application/json'}
+    else:
+        return jsonify(message='Incorrect HTTP verb'), 405
+
+
+# READ: Fetch details of a person
 @app.route('/api/<int:user_id>', methods=['GET'])
 def read(user_id: int):
     person = Person.query.get(user_id)
