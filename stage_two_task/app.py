2b3f58fc8f1760d2ae88b38dede3ecb2d34c60af
Mimi97-aqua <ebenyemiriam17@gmail.com>
2023-09-14 17:04:31 +0100

    Implemented API to get details of everyone in the db

--- stage_two_task/app.py
+++ stage_two_task/app.py
@@ -1,5 +1,31 @@
-from flask import Flask
-from data.resource import app
+from flask import jsonify
+from data.resource import *
+from flask_marshmallow import Marshmallow
+import json
 
+# Initialization
+ma = Marshmallow(app)
+
+
+# Class definition for Marshmallow Person Schema
+class PersonSchema(ma.Schema):
+    class Meta:
+        fields = ('user_id', 'name', 'gender', 'email', 'age', 'weight')
+
+
+person_schema = PersonSchema()
+persons_schema = PersonSchema(many=True)
+
+
+# Fetch details of a person
+@app.route('/api', methods=['GET'])
+def read():
+    persons = Person.query.all()
+    result = persons_schema.dump(persons)
+    result = json.dumps(result, indent=4)
+    return result, 200, {'Content-Type': 'application/json'}
+
+
+# print(app.url_map)
 if __name__ == "__main__":
     app.run()
