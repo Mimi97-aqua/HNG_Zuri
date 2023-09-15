d72b95470f1afd8e5bb5599e88eb60c413209170
Mimi97-aqua <ebenyemiriam17@gmail.com>
2023-09-15 23:34:29 +0100

    Changes

--- stage_two_task/app.py
+++ stage_two_task/app.py
@@ -1,42 +1,79 @@
 from flask import jsonify, request
 from data.resource import *
 import json
-import streamlit as st
 
-def main():
-    st.tile('My APi')
 
-    # CREATE: Adding a new person
-    @app.route('/api', methods=['POST'])
-    def create():
-        if request.method == "POST":
-            if request.is_json:
-                data = request.get_json()
-                name = data.get('name')
-            else:
-                name = request.args.get('name')
-
-            if name:
-                new_person = Person(name=name)
-                db.session.add(new_person)
-                db.session.commit()
-
-                data = new_person.serialize()
-                result = json.dumps(data, indent=4)
-                return result, 201, {'Content-Type': 'application/json'}
-            else:
-                return jsonify(message='Name is required'), 400
+# CREATE: Adding a new person
+@app.route('/api', methods=['POST'])
+def create():
+    if request.method == "POST":
+        if request.is_json:
+            data = request.get_json()
+            name = data.get('name')
         else:
-            return jsonify(message='Incorrect HTTP verb'), 405
+            name = request.args.get('name')
+
+        if name:
+            new_person = Person(name=name)
+            db.session.add(new_person)
+            db.session.commit()
+
+            data = new_person.serialize()
+            result = json.dumps(data, indent=4)
+            return result, 201, {'Content-Type': 'application/json'}
+        else:
+            return jsonify(message='Name is required'), 400
+    else:
+        return jsonify(message='Incorrect HTTP verb'), 405
 
 
-    # READ: Fetch details of a person
-    # UPDATE: Modify details of a person
-    # DELETE: Removing a person
-    @app.route('/api', methods=['GET', 'PUT', 'DELETE'])
-    def rud_operations():
-        user_id = request.args.get('user_id')
-        name = request.args.get('name')
+# READ: Fetch details of a person
+# UPDATE: Modify details of a person
+# DELETE: Removing a person
+@app.route('/api', methods=['GET', 'PUT', 'DELETE'])
+def rud_operations():
+    user_id = request.args.get('user_id')
+    name = request.args.get('name')
+
+    person = None
+
+    if user_id:
+        person = Person.query.get(user_id)
+    elif name:
+        person = Person.query.filter_by(name=name).first()
+
+    if request.method == 'GET':
+        if person:
+            data = person.serialize()
+            result = json.dumps(data, indent=4)
+            return result, 200, {'Content-Type': 'application/json'}
+        else:
+            return jsonify(message='Person matching these values does not exist.'), 401
+    elif request.method == 'PUT':
+        if person:
+            # Update info
+            user_id = request.json.get('user_id')
+            name = request.json.get('name')
+
+            # Modify info
+            person.name = name
+
+            # Save changes to db
+            db.session.commit()
+
+            data = person.serialize()
+            result = json.dumps(data, indent=4)
+            return result, 200, {'Content-Type': 'application/json'}
+        else:
+            return jsonify(message='Person does not exist'), 400
+    elif request.method == 'DELETE':
+        if request.is_json:
+            data = request.get_json()
+            user_id = data.get("user_id")
+            name = data.get('name')
+        else:
+            user_id = request.args.get("user_id")
+            name = request.args.get('name')
 
         person = None
 
@@ -45,51 +82,11 @@ def main():
         elif name:
             person = Person.query.filter_by(name=name).first()
 
-        if request.method == 'GET':
-            if person:
-                data = person.serialize()
-                result = json.dumps(data, indent=4)
-                return result, 200, {'Content-Type': 'application/json'}
-            else:
-                return jsonify(message='Person matching these values does not exist.'), 401
-        elif request.method == 'PUT':
-            if person:
-                # Update info
-                user_id = request.json.get('user_id')
-                name = request.json.get('name')
-
-                # Modify info
-                person.name = name
-
-                # Save changes to db
-                db.session.commit()
-
-                data = person.serialize()
-                result = json.dumps(data, indent=4)
-                return result, 200, {'Content-Type': 'application/json'}
-            else:
-                return jsonify(message='Person does not exist'), 400
-        elif request.method == 'DELETE':
-            if request.is_json:
-                data = request.get_json()
-                user_id = data.get("user_id")
-                name = data.get('name')
-            else:
-                user_id = request.args.get("user_id")
-                name = request.args.get('name')
-
-            person = None
-
-            if user_id:
-                person = Person.query.get(user_id)
-            elif name:
-                person = Person.query.filter_by(name=name).first()
-
-            db.session.delete(person)
-            db.session.commit()
-            return jsonify(message='Person has been successfully deleted.'), 202
-        else:
-            return jsonify(message='Incorrect HTTP verb'), 405
+        db.session.delete(person)
+        db.session.commit()
+        return jsonify(message='Person has been successfully deleted.'), 202
+    else:
+        return jsonify(message='Incorrect HTTP verb'), 405
 
 
 # print(app.url_map)
